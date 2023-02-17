8738315ba6f0664c6264e8bb69c359a9bcb9a179
alessandrini11 <58996105+alessandrini11@users.noreply.github.com>
2023-02-17 10:22:49 +0100

    added front office

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,24 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/a-propos', name: 'app_about', methods: 'GET')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig');
+    }
+
+    #[Route('/mentions-legales', name: 'app_mentions', methods: 'GET')]
+    public function mentions(): Response
+    {
+        return $this->render('home/mentions.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact', methods: 'GET')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig');
     }
 }
