d531a56a4a979723509063d271c449e4caf81b3e
alessandrini11 <58996105+alessandrini11@users.noreply.github.com>
2023-02-17 12:04:45 +0100

    refont register form UI

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,7 +5,11 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,29 +21,76 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
+            ->add('lastname', TextType::class, [
+                'label' => false,
+                'required' => true,
+                'trim' => true,
+                'attr' => [
+                    'class' => 'rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5',
+                    'placeholder' => 'Nom'
+                ]
             ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+            ->add('firstname', TextType::class, [
+                'label' => false,
+                'required' => true,
+                'trim' => true,
+                'attr' => [
+                    'class' => 'rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5',
+                    'placeholder' => 'Prenom'
+                ]
+            ])
+
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'required' => true,
+                'trim' => true,
+                'attr' => [
+                    'class' => 'rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5',
+                    'placeholder' => 'Email'
+                ]
+            ])
+            ->add('telephone', TelType::class, [
+                'label' => false,
+                'required' => true,
+                'trim' => true,
+                'attr' => [
+                    'class' => 'rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5',
+                    'placeholder' => 'Prenom'
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
                 'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} characters',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
+                    ]),
+
+                ],
+                'required' => true,
+                'first_options'  => [
+                    'label' => false,
+                    'attr' => [
+                        'class' => 'rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5',
+                        'placeholder' => 'Mot de Passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'class' => 'rounded-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5',
+                        'placeholder' => 'Confirmation Mot de Passe'
+                    ]
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'Approuver les termes',
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez approuver les termes',
                     ]),
                 ],
             ])
