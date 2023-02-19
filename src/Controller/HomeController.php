38cef33462d8a813038f4ca471933f88034a887c
alessandrini11 <58996105+alessandrini11@users.noreply.github.com>
2023-02-19 17:07:06 +0100

    configure dsn for sending email

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,13 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(MailerInterface $mailer): Response
     {
+        $email = (new Email())
+            ->from('contact@schuamealexandre.com')
+            ->to('alexandreschuame@gmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+//        $mailer->send($email);
+
         return $this->render('home/index.html.twig');
     }
 
